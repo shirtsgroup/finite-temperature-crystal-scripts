1c1ff33ac99cefdca5753432d2466fbc0e8dd800
Nate <nathan.abraham@colorado.edu>
2018-09-20 13:32:06 -0600

    updated for better formatting removing os.system and replacing with subprocess.call adding more commenting

--- PSCP/setup-scripts/addboxvectors.py
+++ PSCP/setup-scripts/addboxvectors.py
@@ -1,63 +1,64 @@
 #/usr/bin/python
-#PYTHON SCRIPT TO ADD THE CORRECT BOX VECTORS TO A GRO FILE GIVEN A TINKER XYZ FILE
-#param x - The name of the .gro file
-#param n - The scaling factor to apply to each xyz coordinate
+# PYTHON SCRIPT TO ADD THE CORRECT BOX VECTORS TO A GRO FILE GIVEN A TINKER XYZ FILE
+# param x - The name of the .gro file
+# param n - The scaling factor to apply to each xyz coordinate
 
-import numpy # numerical array library
-from optparse import OptionParser # for parsing command-line options
+import numpy as np
+from optparse import OptionParser
 import os
-import pdb
 
 #=============================================================================================
 # READ IN USER INPUTS
 #=============================================================================================
 parser = OptionParser()
-parser.add_option('-g', dest = 'grofile', help = 'Input gro file', default='frame.gro')
-parser.add_option('-x', dest = 'xyzfile', help = 'Input xyz file', default='frame.xyz')
-parser.add_option('-s', dest = 'num' , help = 'scaling factor', default='0.1')
+parser.add_option('-g', dest='grofile', help='Input Gromacs file (.gro)', default='frame.gro')
+parser.add_option('-x', dest='xyzfile', help='Input Tinker file (.xyz)', default='frame.xyz')
+#parser.add_option('-s', dest='scaling_factor', help='scaling factor to change the units', default='0.1')
 (options, args) = parser.parse_args()
-scaling_factor = float(options.num)
+#scaling_factor = float(options.scaling_factor)
 
-fname = options.xyzfile
+# If we add in another program than this may need to be changed somehow
+scaling_factor = 0.1
 
-infile = open(fname, 'r')
+xyzfile = options.xyzfile
+grofile = options.grofile
+
+# Reading in the Tinker coordinate file
+infile = open(xyzfile, 'r')
 lines = infile.readlines()
 infile.close()
 
-#Read in the second line of the xyz file
+# Read in the second line of the xyz file
 tokens = lines[1].split()
 
-A=float(tokens[0])
-B=float(tokens[1])
-C=float(tokens[2])
-Alpha=float(tokens[3])
-Beta=float(tokens[4])
-Gamma=float(tokens[5])
+# Tinker lattice vectors
+A = float(tokens[0])
+B = float(tokens[1])
+C = float(tokens[2])
 
-#Calculate the box vectors
-V1x=float(A)
-V1y=0.0
-V1z=0.0
-V2x=B*numpy.cos(Gamma*numpy.pi/180)
-V2y=B*numpy.sin(Gamma*numpy.pi/180)
-V2z=0.0
-V3x=C*numpy.cos(Beta*numpy.pi/180)
-V3y=C*numpy.cos(Alpha*numpy.pi/180)*numpy.sin(Gamma*numpy.pi/180)
-V3z=float(C**2 - V3x**2 - V3y**2)**0.5
+# Tinker lattice angles
+Alpha = float(tokens[3])
+Beta = float(tokens[4])
+Gamma = float(tokens[5])
 
-#Apply the scaling factor to go from A to nm
-V1x=V1x*scaling_factor
-V2x=V2x*scaling_factor
-V2y=V2y*scaling_factor
-V3x=V3x*scaling_factor
-V3y=V3y*scaling_factor
-V3z=V3z*scaling_factor
+# Calculate the box vectors in tensor form and applying the scaling factor to convert from Ang. to nm
+V1x = float(A) * scaling_factor
+V1y = 0.0
+V1z = 0.0
+V2x = B * np.cos(Gamma * np.pi / 180) * scaling_factor
+V2y = B * np.sin(Gamma * np.pi / 180) * scaling_factor
+V2z = 0.0
+V3x = C*np.cos(Beta * np.pi / 180) * scaling_factor
+V3y = C*np.cos(Alpha * np.pi / 180) * np.sin(Gamma * np.pi / 180) * scaling_factor
+V3z = float(C ** 2 - V3x ** 2 - V3y ** 2) ** 0.5 * scaling_factor
 
-#Write the last line to the file
-if Alpha==90.0 and Beta==90.0 and Gamma==90.0:
-    newstr=str("%.8f" % V1x) + " " + str("%.8f" % V2y) + " " + str("%.8f" % V3z)
+# Write the last line to the .gro file
+if Alpha == 90.0 and Beta == 90.0 and Gamma == 90.0:
+    newstr = str("%.8f" % V1x) + " " + str("%.8f" % V2y) + " " + str("%.8f" % V3z)
 else: 
-    newstr=str("%.8f" % V1x) + " " + str("%.8f" % V2y) + " " + str("%.8f" % V3z) + " " + str("%.8f" % V1y) + " " + str("%.8f" % V1z) + " " + str("%.8f" % V2x) + " " + str("%.8f" % V2z) + " " + str("%.8f" % V3x) + " " + str("%.8f" %V3y)
-commandstring = "sed -i 's/0.00000000   0.00000000   0.00000000/ " + newstr + "/g' " + options.grofile
+    newstr = str("%.8f" % V1x) + " " + str("%.8f" % V2y) + " " + str("%.8f" % V3z) + " " + str("%.8f" % V1y) + " " + \
+             str("%.8f" % V1z) + " " + str("%.8f" % V2x) + " " + str("%.8f" % V2z) + " " + str("%.8f" % V3x) + " " + \
+             str("%.8f" % V3y)
+commandstring = "sed -i 's/0.00000000   0.00000000   0.00000000/ " + newstr + "/g' " + grofile
 os.system(commandstring)
 
