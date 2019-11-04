getting the correct sign

PSCP/analysis-scripts/dA_Gamma_MBAR.py
@@ -236,7 +236,6 @@ def dA_Gamma_MBAR(plot_out=True, MINGAMMA=0, MAXGAMMA=100, GSPACING=10, LAMBDA=1
     
         # extract self-consistent weights and uncertainties
         (df_i, ddf_i, theta_i) = mbar.getFreeEnergyDifferences()
-        df_i = - df_i
  
         print("Free Energies Optained...")
     
@@ -244,7 +243,7 @@ def dA_Gamma_MBAR(plot_out=True, MINGAMMA=0, MAXGAMMA=100, GSPACING=10, LAMBDA=1
         df_i /= (beta_k[0] * float(Independent))
         ddf_i /= (beta_k[0] * float(Independent))
     
-        dA[i, :] = df_i[0]
+        dA[i, :] = df_i[-1]
 
         # =============================================================================================
         # COMPUTE UNCERTAINTY USING THE UNCORRELATED DATA
@@ -285,12 +284,13 @@ def dA_Gamma_MBAR(plot_out=True, MINGAMMA=0, MAXGAMMA=100, GSPACING=10, LAMBDA=1
         df_u /= (beta_k[0] * float(Independent))
         ddf_u /= (beta_k[0] * float(Independent))
     
-        ddA[i, :] = ddf_u[0]
+        ddA[i, :] = ddf_u[-1]
         
         # Write out free energy differences
         print("Free Energy Difference (in units of kcal/mol)")
+        print("  dA(Gamma) = A(Gamma) - A(Interactions Off)")
         for k in range(Kbig):
-            print("%8.3f %8.3f" % (df_i[k, 0], ddf_u[k, 0]))
+            print("%8.3f %8.3f" % (df_i[k, -1], ddf_u[k, -1]))
 
     # =============================================================================================
     # PRINT THE FINAL DATA
@@ -299,46 +299,46 @@ def dA_Gamma_MBAR(plot_out=True, MINGAMMA=0, MAXGAMMA=100, GSPACING=10, LAMBDA=1
     out_dA = np.zeros(len(polymorph))
     out_ddA = np.zeros(len(polymorph))
     for i, poly in enumerate(polymorph):
-        out_dA[i] = dA[i, Kbig - 1]
-        out_ddA[i] = ddA[i, Kbig - 1]
+        out_dA[i] = dA[i, 0] #Kbig - 1]
+        out_ddA[i] = ddA[i, 0] #Kbig - 1]
 
     # =============================================================================================
     # PLOT THE FINAL DATA
     # =============================================================================================
     
-    if (plot_out) and polymorphs == 'all':
-        # now plot the free energy change as a function of temperature
-        fig = plt.figure(4)
-        ax = fig.add_subplot(111)
-        xlabel = 'Interaction Strength, $\gamma$'
-        ylabel = 'Relative Free Energy (kcal/mol)'
-        plt.xlabel(xlabel)
-        plt.ylabel(ylabel)
-        Xaxis = [float(j / 100.0) for j in Gammas]
-       
-        if os.path.isfile('BootstrapStd_' + PotNAME + '_Polymorph1_' + str(Molecules) + '_' + Tname + '_' + Pname +
-                                  '_dAvsG_All'):
-            ddA[0, :] = MBARBootstrap.ExtractBootstrap('BootstrapStd_' + PotNAME + '_Polymorph1_' + str(Molecules) + '_' +
-                                                       Tname + '_' + Pname + '_dAvsG_All')
-        elif os.path.isfile('BootstrapStd_' + PotNAME + '_Polymorph2_' + str(Molecules) + '_' + Tname + '_' + Pname +
-                                    '_dAvsG_All'):
-            ddA[1, :] = MBARBootstrap.ExtractBootstrap('BootstrapStd_' + PotNAME + '_Polymorph2_' + str(Molecules) + '_' +
-                                                       Tname + '_' + Pname + '_dAvsG_All')
-        elif os.path.isfile('BootstrapStd_' + PotNAME + '_Polymorph2_' + str(Molecules) + '_' + Tname + '_' + Pname +
-                                    '_dAvsG_All'):
-            ddA[2, :] = MBARBootstrap.ExtractBootstrap('BootstrapStd_' + PotNAME + '_Polymorph3_' + str(Molecules) + '_' +
-                                                       Tname + '_' + Pname + '_dAvsG_All')
-    
-        ax.errorbar(Xaxis, dA[0, :], color='b', yerr=ddA[0, :], label='Benzene I')
-        ax.errorbar(Xaxis, dA[1, :], color='g', yerr=ddA[1, :], label='Benzene II')
-        ax.errorbar(Xaxis, dA[2, :], color='r', yerr=ddA[2, :], label='Benzene III')
-        plt.legend(loc='upper right')
-
-        if len(hinges) > 1:
-            filename = PotNAME + '_' + str(Molecules) + '_' + Tname + '_dAvsG.pdf'
-        else:
-            filename = PotNAME + '_' + str(Molecules) + '_' + Tname + hinge + '_dAvsG.pdf'
-        plt.savefig(filename, bbox_inches='tight')
+#    if (plot_out) and polymorphs == 'all':
+#        # now plot the free energy change as a function of temperature
+#        fig = plt.figure(4)
+#        ax = fig.add_subplot(111)
+#        xlabel = 'Interaction Strength, $\gamma$'
+#        ylabel = 'Relative Free Energy (kcal/mol)'
+#        plt.xlabel(xlabel)
+#        plt.ylabel(ylabel)
+#        Xaxis = [float(j / 100.0) for j in Gammas]
+#       
+#        if os.path.isfile('BootstrapStd_' + PotNAME + '_Polymorph1_' + str(Molecules) + '_' + Tname + '_' + Pname +
+#                                  '_dAvsG_All'):
+#            ddA[0, :] = MBARBootstrap.ExtractBootstrap('BootstrapStd_' + PotNAME + '_Polymorph1_' + str(Molecules) + '_' +
+#                                                       Tname + '_' + Pname + '_dAvsG_All')
+#        elif os.path.isfile('BootstrapStd_' + PotNAME + '_Polymorph2_' + str(Molecules) + '_' + Tname + '_' + Pname +
+#                                    '_dAvsG_All'):
+#            ddA[1, :] = MBARBootstrap.ExtractBootstrap('BootstrapStd_' + PotNAME + '_Polymorph2_' + str(Molecules) + '_' +
+#                                                       Tname + '_' + Pname + '_dAvsG_All')
+#        elif os.path.isfile('BootstrapStd_' + PotNAME + '_Polymorph2_' + str(Molecules) + '_' + Tname + '_' + Pname +
+#                                    '_dAvsG_All'):
+#            ddA[2, :] = MBARBootstrap.ExtractBootstrap('BootstrapStd_' + PotNAME + '_Polymorph3_' + str(Molecules) + '_' +
+#                                                       Tname + '_' + Pname + '_dAvsG_All')
+#    
+#        ax.errorbar(Xaxis, dA[0, :], color='b', yerr=ddA[0, :], label='Benzene I')
+#        ax.errorbar(Xaxis, dA[1, :], color='g', yerr=ddA[1, :], label='Benzene II')
+#        ax.errorbar(Xaxis, dA[2, :], color='r', yerr=ddA[2, :], label='Benzene III')
+#        plt.legend(loc='upper right')
+#
+#        if len(hinges) > 1:
+#            filename = PotNAME + '_' + str(Molecules) + '_' + Tname + '_dAvsG.pdf'
+#        else:
+#            filename = PotNAME + '_' + str(Molecules) + '_' + Tname + hinge + '_dAvsG.pdf'
+#        plt.savefig(filename, bbox_inches='tight')
     return out_dA, out_ddA
 
 

PSCP/analysis-scripts/dA_Lambda_MBAR.py
@@ -259,7 +259,6 @@ def dA_Lambda_MBAR(plot_out=True, MinL=0, MaxL=100, dL=5, GAMMA=100, exponent=4,
     
         # extract self-consistent weights and uncertainties
         (df_i, ddf_i, theta_i) = mbar.getFreeEnergyDifferences()
-        df_i = - df_i
  
         print("Free Energies Optained...")
     
@@ -267,7 +266,7 @@ def dA_Lambda_MBAR(plot_out=True, MinL=0, MaxL=100, dL=5, GAMMA=100, exponent=4,
         df_i /= (beta_k[0] * float(Independent))
         ddf_i /= (beta_k[0] * float(Independent))
     
-        dA[i, :] = df_i[0]
+        dA[i, :] = df_i[-1]
 
         # =============================================================================================
         # COMPUTE UNCERTAINTY USING THE UNCORRELATED DATA
@@ -314,12 +313,13 @@ def dA_Lambda_MBAR(plot_out=True, MinL=0, MaxL=100, dL=5, GAMMA=100, exponent=4,
         df_u /= (beta_k[0] * float(Independent))
         ddf_u /= (beta_k[0] * float(Independent))
     
-        ddA[i, :] = ddf_u[0]
+        ddA[i, :] = ddf_u[-1]
     
         # Write out free energy differences
         print("Free Energy Difference (in units of kcal/mol)")
+        print("  dA(Lambda) = A(Lambda) - A(Fully Restrained)")
         for k in range(Kbig):
-            print("%8.3f %8.3f" % (df_i[k, 0], ddf_u[k, 0]))
+            print("%8.3f %8.3f" % (df_i[k, -1], ddf_u[k, -1]))
 
     # =============================================================================================
     # PRINT THE FINAL DATA
@@ -327,8 +327,8 @@ def dA_Lambda_MBAR(plot_out=True, MinL=0, MaxL=100, dL=5, GAMMA=100, exponent=4,
     out_dA = np.zeros(len(polymorph))
     out_ddA = np.zeros(len(polymorph))
     for i, poly in enumerate(polymorph):
-         out_dA[i] = dA[i, Kbig - 1]
-         out_ddA[i] = ddA[i, Kbig - 1]
+         out_dA[i] = dA[i, 0] #Kbig - 1]
+         out_ddA[i] = ddA[i, 0] #Kbig - 1]
 
     # =============================================================================================
     # PLOT THE FINAL DATA

PSCP/analysis-scripts/dA_endpoint_MBAR.py
@@ -18,7 +18,7 @@ def dA_endpoint_MBAR(polymorphs='p1 p2', Molecules=72, Independent=4, Temp=200):
     for i, poly in enumerate(polymorphs):
         if os.path.isfile(poly + '/interactions/100/PROD.edr') and os.path.isfile(poly + '/interactions/100/END.edr'):
             # Loading in the difference between the endpoint and production files
-            dU = panedr.edr_to_df(poly + '/interactions/100/PROD.edr')['Potential'].values - panedr.edr_to_df(poly + '/interactions/100/END.edr')['Potential'].values
+            dU = panedr.edr_to_df(poly + '/interactions/100/END.edr')['Potential'].values - panedr.edr_to_df(poly + '/interactions/100/PROD.edr')['Potential'].values
 
             # Converting the energy differences to go into pymbar
             dW = dU / Molecules * kJ_to_kcal / (kB * Temp)
@@ -26,7 +26,7 @@ def dA_endpoint_MBAR(polymorphs='p1 p2', Molecules=72, Independent=4, Temp=200):
             # Getting the energy differences with MBAR using Exponential Averaging
             da = np.array(pymbar.EXP(dW)) * kB * Temp
 
-            dA[i] = da[0]
+            dA[i] = -da[0]
             ddA[i] = da[1]
         else:
             dA[i] = np.nan